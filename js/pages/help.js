58234fb2f005a34a4cc28da9e591b1a48deaa027
bortoli94 <gbortoli.rosario@hotmail.com>
2022-07-07 18:52:14 -0300

    correccion help

--- js/pages/help.js
+++ js/pages/help.js
@@ -8,11 +8,14 @@ const contactForm = 'template_nv5khrm';
 const successEmail = '<div class="status success"><span id="bold">Email</span> enviado exitosamente</div>';
 const errorEmail = '<div class="status error">Ingrese un <span id="bold">Email</span> válido</div>';
 const errorEmailApi = '<div class="status error">Algo fallo en el envío, por favor intente mas tarde</div>';
+const errorInputs = '<div class="status error">Debe completar todos los campos</div>';
 //**************************CARTELES STATUS EMAIL*****************************************************
 
 //**************************ELEMENTOS DOM*************************************************************
 const statusEmail = document.getElementById("statusEmail"); //seccion donde se publica el status del Email
 const emailValue = document.getElementById("email"); //Input email
+const nameValue = document.getElementById("name"); //Inpunt nombre
+const messaje = document.getElementById("messaje");//Input Mensaje
 const form = document.getElementById("contact-form"); //formulario
 //****************************************************************************************************
 
@@ -23,18 +26,21 @@ window.onload = function() {
         
         statusEmail.innerHTML = loader;                
         
-        if (validateEmail(emailValue.value)) {
+        if (validateEmail(emailValue.value) && nameValue.value != "" && messaje.value != "") {
             event.preventDefault();
-            emailjs.sendForm(contactService, contactForm, this)
-                .then(function() {
-                    setTimeout(function() {statusEmail.innerHTML = successEmail;},1500);
-                }, function(error) {
-                    setTimeout(function() {statusEmail.innerHTML = errorEmailApi;},1500);
-                });
+            emailjs.sendForm(contactService, contactForm, this)            
+            .then(function() {
+                statusEmail.innerHTML = successEmail;
+            }, function(error) {
+                statusEmail.innerHTML = errorEmailApi;
+            });
         }else{
-            setTimeout(function() {statusEmail.innerHTML = errorEmail;},1500);
-        }
-        setTimeout(function() {removeStatus[0].remove();},10000);  
+            if (nameValue.value == "" || messaje.value == "") {
+                statusEmail.innerHTML = errorInputs;
+            }else{
+                statusEmail.innerHTML = errorEmail;
+            }
+        } 
     });}
     
 
